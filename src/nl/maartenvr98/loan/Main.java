70900c1a279969a943e3f1ac10835d52adef3b81
Maarten van Rijssel <maartenrijssel@hotmail.com>
2017-12-05 15:56:35 +0100

    Initial commit

---
+++ src/nl/maartenvr98/loan/Main.java
@@ -0,0 +1,13 @@
+package nl.maartenvr98.loan;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin enabled");
+
+    }
+
+}
